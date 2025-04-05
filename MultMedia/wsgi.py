f7a9f078ecdfee6605d31effdf4eec0e0d0f80d2
afidhu <afidhumatata81@gmail.com>
2025-04-05 23:09:08 +0300

    first commit

--- MultMedia/wsgi.py
+++ MultMedia/wsgi.py
@@ -7,7 +7,11 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
+import sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 import os
+
 
 from django.core.wsgi import get_wsgi_application
 
